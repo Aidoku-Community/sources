479598bb3c18f140b10eb4c1a0203a6237c5a8ca
alexa <alexanderlee014@gmail.com>
2025-08-12 23:26:58 -0700

    Continued adding to Home Page component of source

--- sources/en.mangapark/src/lib.rs
+++ sources/en.mangapark/src/lib.rs
@@ -1,7 +1,7 @@
 #![no_std]
 use aidoku::{
-	alloc::{vec, String, Vec},
-	imports::{defaults::defaults_get, net::Request},
+	alloc::{string::ToString, vec, String, Vec},
+	imports::{html::*, net::Request},
 	prelude::*, 
 	AidokuError, Chapter, DeepLinkHandler, DeepLinkResult, FilterValue, Home, HomeComponent, HomeLayout, Listing, ListingProvider, Manga, MangaPageResult, MangaWithChapter, Page, Result, Source
 };
@@ -14,7 +14,7 @@ use aidoku::{
 
 const BASE_URL: &str = "https://mangapark.com";
 
-const PAGE_SIZE: i32 = 20;
+const PAGE_SIZE: i32 = 16;
 
 struct MangaPark;
 
@@ -73,8 +73,6 @@ impl Source for MangaPark {
 				.collect::<Vec<Manga>>()
 		})
 		.unwrap_or_default();		
-		// aidoku::prelude::println!("HTML: {}", html.select_first(MANGA_SELECTOR).);
-		// aidoku::println!("First few chars: {}", &html.read()[..100.min(html.read().len())]);
 		Ok(MangaPageResult {
 			entries: Vec::new(),
 			has_next_page: false,
@@ -151,11 +149,11 @@ impl Source for MangaPark {
 		// what is the page component suppose to be?
 		// let url = format!("{}/search/{}-", BASE_URL,page,);
 // 
-		let manga_url = format!("{BASE_URL}{}", manga.key);
-		println!("{}",manga_url);
-		if needs_details{
-			let html = Request::get(&manga_url)?.html()?;
-		}
+		// let manga_url = format!("{BASE_URL}{}", manga.key);
+		// println!("{}",manga_url);
+		// if needs_details{
+		// 	let html = Request::get(&manga_url)?.html()?;
+		// }
 
 		if needs_chapters{
 
@@ -175,108 +173,91 @@ impl ListingProvider for MangaPark {
 	}
 }
 
+// This is the home page, which can contain various components like scrollers, manga lists, etc.
 impl Home for MangaPark {
 	fn get_home(&self) -> Result<HomeLayout> {
-		let entries = self.get_search_manga_list(None, 1, Vec::new())?.entries;
-		let chapter = Chapter {
-			key: String::from("1"),
-			chapter_number: Some(1.0),
-			title: Some(String::from("Chapter")),
-			date_uploaded: Some(1692318525),
-			..Default::default()
-		};
-		let manga_chapters = entries
-			.iter()
-			.map(|manga| MangaWithChapter {
-				manga: manga.clone(),
-				chapter: chapter.clone(),
-			})
-			.take(3)
-			.collect::<Vec<_>>();
+		let html = Request::get(BASE_URL)?.html()?;
+		//parent: er_0
+		const MEMBER_UPLOADS_SELECTOR: &str = r#"div[q\:key="er_0"]"#;
+		//parent: Xs_4
+		const LATEST_UPDATES_SELECTOR: &str = r#"div[q\:key="MY_0"]"#;
+
+		const CHAPTER_SELECTOR: &str = r#"div[q\:key="R7_8"]"#;
+
+		fn parse_manga_with_chapter(element: &Element)-> Option<MangaWithChapter>{
+			let links = element.select_first("a")?;
+			let manga_title = element.select("h3 span")?.text()?;
+			let cover = element.select_first("img")?.attr("abs:src"); 
+			let manga_url = links.attr("abs:href");
+			println!("{}", links.attr("abs:href").unwrap_or_default());
+			let manga_key = links.attr("abs:href")?.strip_prefix("/title")?.strip_suffix("/en")?.into();
+
+
+			// let chapter_key = 
+			Some(MangaWithChapter 
+				{ 
+					manga: Manga{ 
+						key: manga_key,
+						title: manga_title,
+						cover,
+						url: manga_url,
+						// content_rating: , // NSFW if Doujinshi... 
+						..Default::default()
+					}, 
+					chapter: Chapter{
+						// key: ,
+						//title: ,
+						//chapter_number: ,
+						//date_uploaded: ,
+
+						..Default::default()
+					}
+				})
+		}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+		let member_uploads = html
+		.select(MEMBER_UPLOADS_SELECTOR)
+		.map(|elment_list| {
+			elment_list.filter_map(|element| parse_manga_with_chapter(&element))
+				.collect::<Vec<_>>()
+		})
+		.unwrap_or_default();
+
+		// let latest_updates = html
+		// 	.select("section:has(h2:contains(Latest Updates)) article")
+		// 	.map(|els| {
+		// 		els.filter_map(|el| parse_manga_with_chapter(&el))
+		// 			.collect::<Vec<_>>()
+		// 	})
+		// 	.unwrap_or_default();
+
+
 		Ok(HomeLayout {
 			components: vec![
 				HomeComponent {
-					title: Some(String::from("Big Scroller")),
+					title: Some(("Member Uploads").into()),
 					subtitle: None,
-					value: aidoku::HomeComponentValue::BigScroller {
-						entries: entries.clone(),
-						auto_scroll_interval: Some(10.0),
-					},
+					value: aidoku::HomeComponentValue::MangaChapterList 
+					{ 
+						page_size: Some(PAGE_SIZE),
+						entries: member_uploads, 
+						listing: None
+					} 
 				},
 				HomeComponent {
-					title: Some(String::from("Manga Chapter List")),
+					title: Some(("Latest Releases").into()),
 					subtitle: None,
-					value: aidoku::HomeComponentValue::MangaChapterList {
-						page_size: None,
-						entries: manga_chapters,
-						listing: None,
-					},
-				},
-				HomeComponent {
-					title: Some(String::from("Manga List")),
-					subtitle: None,
-					value: aidoku::HomeComponentValue::MangaList {
-						ranking: false,
-						page_size: None,
-						entries: entries.iter().take(2).cloned().map(|m| m.into()).collect(),
-						listing: None,
-					},
-				},
-				HomeComponent {
-					title: Some(String::from("Manga List (Paged, Ranking)")),
-					subtitle: None,
-					value: aidoku::HomeComponentValue::MangaList {
-						ranking: true,
-						page_size: Some(3),
-						entries: entries.iter().take(8).cloned().map(|m| m.into()).collect(),
-						listing: None,
-					},
-				},
-				HomeComponent {
-					title: Some(String::from("Scroller")),
-					subtitle: None,
-					value: aidoku::HomeComponentValue::Scroller {
-						entries: entries.clone().into_iter().map(|m| m.into()).collect(),
-						listing: None,
-					},
-				},
-				HomeComponent {
-					title: Some("Filters".into()),
-					subtitle: None,
-					value: aidoku::HomeComponentValue::Filters(vec![
-						aidoku::FilterItem::from(String::from("Action")),
-						"Adventure".into(),
-						"Fantasy".into(),
-						"Horror".into(),
-						"Slice of Life".into(),
-						"Magic".into(),
-						"Adaptation".into(),
-					]),
-				},
-				HomeComponent {
-					title: Some(String::from("Links")),
-					subtitle: None,
-					value: aidoku::HomeComponentValue::Links(vec![
-						aidoku::Link {
-							title: String::from("Website Link"),
-							value: Some(aidoku::LinkValue::Url(String::from("https://aidoku.app"))),
+					value: aidoku::HomeComponentValue::MangaChapterList 
+					{ 
+						page_size: Some(PAGE_SIZE),
+						entries: Vec::new(), 
+						listing: Some(Listing { 
+							id: "latest".into(),
+							name: "Latest Releases".into(),
 							..Default::default()
-						},
-						aidoku::Link {
-							title: String::from("Manga Link"),
-							value: Some(aidoku::LinkValue::Manga(entries.first().unwrap().clone())),
-							..Default::default()
-						},
-						aidoku::Link {
-							title: String::from("Listing Link"),
-							value: Some(aidoku::LinkValue::Listing(Listing {
-								id: String::from("listing"),
-								name: String::from("Listing"),
-								kind: aidoku::ListingKind::List,
-							})),
-							..Default::default()
-						},
-					]),
+						}) 
+					},
 				},
 			],
 		})
