3e1746e2eb44803b9d1144897607d1172a63cea6
alexa <alexanderlee014@gmail.com>
2025-08-31 12:20:53 -0700

    Created Filter helper class that defines a function to handle filters into a query string

--- sources/en.mangapark/src/filter.rs
+++ sources/en.mangapark/src/filter.rs
@@ -1,12 +1,13 @@
 // use crate::helper;
 // use crate::model::SortOptions;
 use aidoku::{
-	alloc::{borrow::ToOwned, String, Vec},
+	alloc::{ string::ToString, String, Vec},
 	helpers::uri::QueryParameters,
 	prelude::*,
 	FilterValue,
 };
 
+use crate::model::SortOptions;
 /* FilterValues
  * Add all filters to corresponding value(ex: Sort: sort(option), order(ascending))
 
@@ -25,62 +26,55 @@ use aidoku::{
 pub fn get_filters(query: Option<String>, filters: Vec<FilterValue>) -> String {
 	let mut qs = QueryParameters::new();
 
-	// if let Some(query) = query {
-	// 	let cleaned = helper::remove_special_chars(query).trim().to_owned();
-	// 	if !cleaned.is_empty() {
-	// 		qs.push("text", Some(&cleaned));
-	// 	}
-	// }
-
-	let mut extra_params = String::new();
-
+	if query.is_some() {
+		qs.push("word", query.as_deref()); 
+	}
 	for filter in filters {
 		match filter {
-            // ?
-			FilterValue::Text { ref value, .. } => {
-				if !value.is_empty() {
-					qs.push("author", Some(value));
-				}
-			}
-			FilterValue::Sort {
-				id, index, ascending, ..
-			} => {
-				let option: &str = SortOptions::from(index).into();
-				qs.push("sort", Some(option));
-				qs.push(
-					"order",
-					Some(if ascending { "Ascending" } else { "Descending" }),
-				);
-			}
 			FilterValue::MultiSelect {
 				ref id,
 				ref included,
 				ref excluded,
 			} => {
 				if id == "genre" {
-					for tag in included {
-						qs.push("included_tag", Some(tag));
+					let mut joined: String = "".to_string();
+					if !included.is_empty() {
+						joined = included.join(",");
+					}	
+					if !excluded.is_empty() { 
+						joined.push_str("|");
+						joined = joined + &excluded.join(",");
 					}
-					for tag in excluded {
-						qs.push("excluded_tag", Some(tag));
-					}
-				} else {
-					for val in included {
-						extra_params.push_str(val);
+					if !included.is_empty() || !excluded.is_empty() {
+						qs.push("genres", Some(&joined));
 					}
 				}
+				else{
+					qs.push("orig", Some(&included.join(",")));
+				}
 			}
-			FilterValue::Check { value, .. } => qs.push(
-				"official",
-				Some(match value {
-					0 => "False",
-					1 => "True",
-					_ => "Any",
-				}),
-			),
+			FilterValue::Select{
+				id, value
+			} => {
+				if id == "original_work_status"{
+					qs.push("status", Some(&value));
+				}
+				if id == "mpark_upload_status" {
+					qs.push("upload", Some(&value));
+				}
+				if id == "chapters"{
+					qs.push("chapters", Some(&value));
+				}
+			}
+			FilterValue::Sort {
+				index, ..
+			} => {
+				let option: &str = SortOptions::from(index).into();
+				qs.push("sortby", Some(option));
+			}
 			_ => {}
 		}
 	}
 
-	format!("{qs}{extra_params}")
+	format!("{qs}")
 }
