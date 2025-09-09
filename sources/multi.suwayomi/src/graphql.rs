34e393ec2f88bb84466a5e1e55a1ba614fa7fbe5
gh-adrl <104177296+gh-adrl@users.noreply.github.com>
2025-09-08 19:27:41 -0700

    Combine fragment and the queries

--- sources/multi.suwayomi/src/graphql.rs
+++ sources/multi.suwayomi/src/graphql.rs
@@ -1,4 +1,3 @@
-use alloc::format;
 use alloc::string::{String, ToString};
 
 pub struct GraphQLQuery {
@@ -6,47 +5,39 @@ pub struct GraphQLQuery {
 	pub query: String,
 }
 
-const MANGA_FRAGMENT: &str = r#"fragment MangaFragment on MangaType {
-	id
-	title
-	thumbnailUrl
-	author
-	artist
-	genre
-	status
-}"#;
-
-const GET_SEARCH_MANGA_LIST_BASE: &str = r#"query GET_SEARCH_MANGA_LIST($condition: MangaConditionInput, $order: [MangaOrderInput!], $filter: MangaFilterInput) {
+const GET_SEARCH_MANGA_LIST: &str = r#"query GET_SEARCH_MANGA_LIST($condition: MangaConditionInput, $order: [MangaOrderInput!], $filter: MangaFilterInput) {
 	mangas(condition: $condition, order: $order, filter: $filter) {
 		nodes {
-			...MangaFragment
+			id
+			title
+			thumbnailUrl
+			author
+			artist
+			genre
+			status
 		}
 	}
 }"#;
 
-const CHAPTER_FRAGMENT: &str = r#"fragment ChapterFragment on ChapterType {
-	id
-	name
-	chapterNumber
-	scanlator
-	uploadDate
-	sourceOrder
-	manga {
-	    source {
-	        displayName
-	    }
-	}
-}"#;
-
-const GET_MANGA_CHAPTERS_BASE: &str = r#"query GET_MANGA_CHAPTERS($mangaId: Int!) {
+const GET_MANGA_CHAPTERS: &str = r#"query GET_MANGA_CHAPTERS($mangaId: Int!) {
 	chapters(condition: {mangaId: $mangaId}, order: [{by: SOURCE_ORDER, byType: DESC}]) {
 		nodes {
-			...ChapterFragment
+			id
+			name
+			chapterNumber
+			scanlator
+			uploadDate
+			sourceOrder
+			manga {
+				source {
+					displayName
+				}
+			}
 		}
 	}
 }"#;
 
-const GET_CHAPTER_PAGES_BASE: &str = r#"mutation GET_PAGE_LIST($input: FetchChapterPagesInput!) {
+const GET_CHAPTER_PAGES: &str = r#"mutation GET_PAGE_LIST($input: FetchChapterPagesInput!) {
 	fetchChapterPages(input: $input) {
 		pages
 	}
@@ -63,21 +54,21 @@ impl GraphQLQuery {
 	pub fn get_search_manga_list() -> Self {
 		Self {
 			operation_name: "GET_SEARCH_MANGA_LIST",
-			query: format!("{}\n\n{}", MANGA_FRAGMENT, GET_SEARCH_MANGA_LIST_BASE),
+			query: GET_SEARCH_MANGA_LIST.to_string(),
 		}
 	}
 
 	pub fn get_manga_chapters() -> Self {
 		Self {
 			operation_name: "GET_MANGA_CHAPTERS",
-			query: format!("{}\n\n{}", CHAPTER_FRAGMENT, GET_MANGA_CHAPTERS_BASE),
+			query: GET_MANGA_CHAPTERS.to_string(),
 		}
 	}
 
 	pub fn get_chapter_pages() -> Self {
 		Self {
 			operation_name: "GET_PAGE_LIST",
-			query: GET_CHAPTER_PAGES_BASE.to_string(),
+			query: GET_CHAPTER_PAGES.to_string(),
 		}
 	}
 
