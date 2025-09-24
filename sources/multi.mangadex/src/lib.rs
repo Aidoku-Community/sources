e6a20a6d2f5c2ac905e7b5238430a29bbb5ae714
c0n <77713736+c0ntens@users.noreply.github.com>
2025-09-24 03:59:51 +0000

    fix(multi.mangadex): add show locked chapters & fix data saver not working

--- sources/multi.mangadex/src/lib.rs
+++ sources/multi.mangadex/src/lib.rs
@@ -221,6 +221,7 @@ impl Source for MangaDex {
 		if needs_chapters {
 			let languages = settings::get_languages_with_key("translatedLanguage")?;
 			let blocked_groups = settings::get_blocked_uuids()?;
+			let show_unavailable_chapters = settings::get_locked_chapters();
 
 			let url = format!(
 				"{API_URL}/manga/{}/feed\
@@ -231,11 +232,13 @@ impl Source for MangaDex {
 					&contentRating[]=erotica\
 					&contentRating[]=suggestive\
 					&contentRating[]=safe\
+					&includeUnavailable={}\
 					&includes[]=user\
 					&includes[]=scanlation_group\
 					{languages}\
 					{blocked_groups}",
-				manga.key
+				manga.key,
+				if show_unavailable_chapters { "1" } else { "0" }
 			);
 
 			let (mut chapters, total) = Request::get(&url)?
