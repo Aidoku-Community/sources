65d91b8127b6b7882381050c7dabe24c56edcdb2
Alex <91232455+Sheepodeepo@users.noreply.github.com>
2025-10-06 17:07:36 -0700

    Update sort function for updated default sort order

--- sources/en.flamecomics/src/filter.rs
+++ sources/en.flamecomics/src/filter.rs
@@ -17,16 +17,14 @@ pub fn get_genre_filter(filters: &Vec<FilterValue>) -> String {
 }
 
 pub fn sort(filters: &Vec<FilterValue>, mut entries: Vec<Manga>) -> Vec<Manga> {
-	let mut sort_by_ascending: bool = true;
 	for filter in filters {
 		if let FilterValue::Sort { ref ascending, .. } = *filter {
-			sort_by_ascending = *ascending;
+			if *ascending {
+				entries.sort_by(|a: &Manga, b: &Manga| b.title.cmp(&a.title));
+			} else {
+				entries.sort_by(|a: &Manga, b: &Manga| a.title.cmp(&b.title));
+			}
 		}
-	}
-	if sort_by_ascending {
-		entries.sort_by(|a, b| a.title.cmp(&b.title));
-	} else {
-		entries.sort_by(|a, b| b.title.cmp(&a.title));
 	}
 	entries
 }
