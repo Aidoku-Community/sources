12a7030f65c9efd0837dc0b346b6d69efab8570e
longthb <tranhoangbaolong0201@gmail.com>
2025-12-15 00:14:31 +0700

    new comix basic layout

--- sources/en.comix/src/lib.rs
+++ sources/en.comix/src/lib.rs
@@ -8,7 +8,7 @@ use aidoku::{
 	prelude::*,
 };
 
-use crate::model::ApiResponse;
+use crate::model::{ComixChapter, ComixManga, ComixResponse};
 
 mod model;
 // use iken::{IKen, Impl, Params};
@@ -34,11 +34,61 @@ impl Source for Comix {
 
 	fn get_manga_update(
 		&self,
-		manga: Manga,
+		mut manga: Manga,
 		needs_details: bool,
 		needs_chapters: bool,
 	) -> Result<Manga> {
-		todo!()
+		if needs_details {}
+
+		if needs_chapters {
+			let limit: i64 = 500;
+			let url = format!(
+				"{API_URL}/manga/{}/chapters?limit={}&page={}&ordern[number]=desc",
+				limit, manga.key, 1
+			);
+			// .header("Referer", &format!("{}/", params.base_url))
+			let res = Request::get(url)?
+				.send()?
+				.get_json::<ComixResponse<ComixChapter>>()?;
+
+			let total = res.result.pagination.total;
+
+			let chapters: Vec<Chapter> = res
+				.result
+				.items
+				.into_iter()
+				.map(|item| {
+					let url = Some(item.url(&manga));
+					let mut ch: Chapter = item.into();
+					ch.url = url; // change 1 field (example)
+					ch
+				})
+				.collect();
+
+			let (mut chapters, total) = (chapters, total);
+			let pages = (total + limit - 1) / limit;
+
+			for page in 2..=pages {
+				let url = format!(
+					"{API_URL}/manga/{}/chapters?limit={limit}&page={page}&ordern[number]=desc",
+					manga.key
+				);
+
+				let res = Request::get(url)?
+					.send()?
+					.get_json::<ComixResponse<ComixChapter>>()?;
+
+				chapters.extend(res.result.items.into_iter().map(|item| {
+					let url = Some(item.url(&manga));
+					let mut ch: Chapter = item.into();
+					ch.url = url; // change 1 field (example)
+					ch
+				}));
+			}
+			manga.chapters = Some(chapters);
+		}
+
+		Ok(manga)
 	}
 
 	fn get_page_list(&self, manga: Manga, chapter: Chapter) -> Result<Vec<Page>> {
@@ -50,15 +100,16 @@ impl Home for Comix {
 	fn get_home(&self) -> Result<HomeLayout> {
 		let url = format!("{API_URL}/manga?order[views_30d]=desc&limit=28");
 
-		let mangaResponse = Request::get(&url)?.send()?.get_json::<ApiResponse>()?;
-		let mangaList = mangaResponse
+		let mut manga_request = Request::get(&url)?.send()?;
+		let manga_response = manga_request.get_json::<ComixResponse<ComixManga>>()?;
+		let manga_list = manga_response
 			.result
 			.items
 			.into_iter()
 			.map(|item| Manga {
-				key: item.manga_id.to_string(),
+				key: item.hash_id.to_string(),
 				title: item.title,
-				cover: item.poster.medium.into(),
+				cover: Some(item.poster.medium.to_string()),
 				..Default::default()
 			})
 			.collect();
@@ -68,7 +119,7 @@ impl Home for Comix {
 				title: Some("Hot Updates".into()),
 				subtitle: None,
 				value: aidoku::HomeComponentValue::BigScroller {
-					entries: mangaList,
+					entries: manga_list,
 					auto_scroll_interval: None,
 				},
 			}],
--- sources/en.comix/src/model.rs
+++ sources/en.comix/src/model.rs
@@ -1,43 +1,44 @@
 // use crate::Params;
 use aidoku::{
-	Chapter, Manga, MangaStatus, Viewer,
-	alloc::{String, Vec, string::ToString, vec},
+	Chapter, ContentRating, Manga, MangaStatus, Viewer,
+	alloc::{String, Vec, fmt, string::ToString, vec},
 	helpers::element::ElementHelpers,
 	imports::html::Html,
 	prelude::*,
 };
-use serde::Deserialize;
+use chrono::DateTime;
+use serde::de::{self, Deserializer, Visitor};
+use serde::{self, Deserialize};
 
 #[derive(Debug, Clone, Deserialize)]
 #[serde(rename_all = "camelCase")]
-// #[serde(bound(deserialize = "'de: 'a"))]
-pub struct ApiResponse {
+pub struct ComixResponse<T> {
 	pub status: i64,
-	pub result: ResultData,
+	pub result: ResultData<T>,
 }
 
 #[derive(Debug, Clone, Deserialize)]
-pub struct ResultData {
-	pub items: Vec<MangaItem>,
+pub struct ResultData<T> {
+	pub items: Vec<T>,
 	pub pagination: Pagination,
 }
 
 #[derive(Debug, Clone, Deserialize)]
-pub struct MangaItem {
+pub struct ComixManga<'a> {
 	pub manga_id: i64,
-	pub hash_id: String,
+	pub hash_id: &'a str,
 	pub title: String,
 	pub alt_titles: Vec<String>,
-	pub synopsis: String,
-	pub slug: String,
+	pub synopsis: &'a str,
+	pub slug: &'a str,
 	pub rank: i64,
 	#[serde(rename = "type")]
-	pub type_: String,
+	pub type_: ComixTypeFilter,
 
-	pub poster: Poster,
+	pub poster: Poster<'a>,
 
 	pub original_language: Option<String>,
-	pub status: String,
+	pub status: ComixStatus,
 
 	pub final_volume: i64,
 	pub final_chapter: i64,
@@ -62,14 +63,44 @@ pub struct MangaItem {
 	pub is_nsfw: bool,
 
 	pub year: i64,
-	pub term_ids: Vec<i64>,
+	pub term_ids: Vec<Tag>,
+}
+
+#[derive(Default, Debug, Clone, Deserialize)]
+pub struct ComixChapter {
+	pub chapter_id: i64,
+	pub manga_id: i64,
+
+	pub scanlation_group_id: i64,
+
+	/// In your JSON it's `0`/`1` (number), not `true`/`false`.
+	pub is_official: i64,
+
+	pub number: i64,
+	pub name: String,
+	pub language: String,
+
+	pub volume: i64,
+	pub votes: i64,
+
+	pub created_at: i64,
+	pub updated_at: i64,
+
+	pub scanlation_group: ScanlationGroup,
+}
+
+#[derive(Default, Debug, Clone, Deserialize)]
+pub struct ScanlationGroup {
+	pub scanlation_group_id: i64,
+	pub name: String,
+	pub slug: String,
 }
 
 #[derive(Debug, Clone, Deserialize)]
-pub struct Poster {
-	pub small: String,
-	pub medium: String,
-	pub large: String,
+pub struct Poster<'a> {
+	pub small: &'a str,
+	pub medium: &'a str,
+	pub large: &'a str,
 }
 
 #[derive(Debug, Clone, Deserialize)]
@@ -88,4 +119,536 @@ pub struct Pagination {
 	pub last_page: i64,
 	pub from: i64,
 	pub to: i64,
+}
+
+impl ComixManga<'_> {
+	pub fn into_basic_manga(self) -> Manga {
+		Manga {
+			key: String::from(self.hash_id),
+			title: self.title().unwrap_or_default(),
+			cover: self.cover(),
+			..Default::default()
+		}
+	}
+
+	pub fn title(&self) -> Option<String> {
+		self.alt_titles
+			.first()
+			.map(|t| t.to_string())
+			.or_else(|| Some(self.title.clone()))
+	}
+
+	pub fn description(&self) -> Option<String> {
+		Some(self.synopsis.into())
+	}
+
+	pub fn cover(&self) -> Option<String> {
+		// self.relationships.iter().find_map(|r| {
+		// 	if r.r#type == "cover_art" {
+		// 		Some(format!(
+		// 			"{COVER_URL}/covers/{}/{}{}",
+		// 			self.id,
+		// 			r.attributes
+		// 				.clone()
+		// 				.and_then(|v| v.get("fileName").map(|v| v.as_str().map(String::from)))
+		// 				.flatten()
+		// 				.unwrap_or_default(),
+		// 			settings::get_cover_quality()
+		// 		))
+		// 	} else {
+		// 		None
+		// 	}
+		// })
+		todo!()
+	}
+
+	pub fn authors(&self) -> Vec<String> {
+		// self.relationships
+		// 	.iter()
+		// 	.filter(|r| r.r#type == "author")
+		// 	.filter_map(|r| {
+		// 		r.attributes
+		// 			.as_ref()
+		// 			.map(|a| a.get("name").map(|v| v.as_str().map(String::from)))
+		// 	})
+		// 	.flatten()
+		// 	.flatten()
+		// 	.collect()
+		todo!()
+	}
+
+	pub fn artists(&self) -> Vec<String> {
+		// self.relationships
+		// 	.iter()
+		// 	.filter(|r| r.r#type == "artist")
+		// 	.filter_map(|r| {
+		// 		r.attributes
+		// 			.as_ref()
+		// 			.map(|a| a.get("name").map(|v| v.as_str().map(String::from)))
+		// 	})
+		// 	.flatten()
+		// 	.flatten()
+		// 	.collect()
+		todo!()
+	}
+
+	pub fn url(&self) -> String {
+		format!("https://comix.com/title/{}", self.hash_id)
+	}
+
+	pub fn tags(&self) -> Vec<String> {
+		self.term_ids
+			.iter()
+			// .filter_map(|t| t.attributes.name.get())
+			.map(|t| t.to_string())
+			.collect()
+	}
+
+	pub fn status(&self) -> MangaStatus {
+		match self.status {
+			ComixStatus::Releasing => MangaStatus::Ongoing,
+			ComixStatus::Finished => MangaStatus::Completed,
+			ComixStatus::OnHiatus => MangaStatus::Hiatus,
+			ComixStatus::Discontinued => MangaStatus::Cancelled,
+			ComixStatus::NotYetReleased => MangaStatus::Unknown,
+		}
+	}
+
+	pub fn content_rating(&self) -> ContentRating {
+		if self.is_nsfw {
+			ContentRating::NSFW
+		} else {
+			ContentRating::Safe
+		}
+	}
+}
+
+impl From<ComixManga<'_>> for Manga {
+	fn from(val: ComixManga<'_>) -> Self {
+		let tags = val.tags();
+		let viewer = match val.type_ {
+			ComixTypeFilter::Manga => Viewer::RightToLeft,
+			ComixTypeFilter::Manhwa => Viewer::Webtoon,
+			ComixTypeFilter::Manhua => Viewer::Vertical,
+			_ => Viewer::RightToLeft,
+		};
+
+		Manga {
+			key: String::from(val.hash_id),
+			title: val.title().unwrap_or_default(),
+			cover: val.cover(),
+			artists: Some(val.artists()),
+			authors: Some(val.authors()),
+			description: val.description(),
+			url: Some(val.url()),
+			tags: Some(tags),
+			status: val.status(),
+			content_rating: val.content_rating(),
+			viewer,
+			..Default::default()
+		}
+	}
+}
+
+impl ComixChapter {
+	pub fn has_external_url(&self) -> bool {
+		false
+	}
+
+	pub fn url(&self, manga: &Manga) -> String {
+		match manga.url.as_deref() {
+        Some(base) => format!("{}/{}", base, self.chapter_id),
+        None => String::new(),
+    }
+	}
+
+	pub fn manga_id(&self) -> Option<i64> {
+		self.chapter_id.into()
+	}
+
+	pub fn scanlators(&self) -> Vec<String> {
+		vec![self.scanlation_group.name.clone()]
+	}
+}
+
+impl From<ComixChapter> for Chapter {
+	fn from(val: ComixChapter) -> Self {
+		let chapter_number = Some(val.number as f32);
+		let volume_number = Some(val.volume as f32);
+
+		// As per MangaDex upload guidelines, if the volume and chapter are both null or
+		// for serialized entries, the volume is 0 and chapter is null, it's a oneshot.
+		// They should have a title of "Oneshot" but some don't, so we'll add it if it's missing.
+		let title = if (volume_number == Some(0.0)) && val.name.is_empty() {
+			Some(String::from("Oneshot"))
+		} else {
+			Some(val.name.clone())
+		};
+
+		Chapter {
+			key: String::from(val.chapter_id.to_string()),
+			title,
+			chapter_number,
+			volume_number,
+			date_uploaded: Some(val.updated_at),
+			scanlators: Some(val.scanlators()),
+			// url: Some(val.url()),
+			language: Some(String::from(val.language)),
+			..Default::default()
+		}
+	}
+}
+
+#[derive(Deserialize, Debug, Clone)]
+#[serde(rename_all = "snake_case")]
+#[derive(Default)]
+pub enum ComixStatus {
+	Releasing,
+	#[default]
+	Finished,
+	OnHiatus,
+	Discontinued,
+	NotYetReleased,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+#[serde(rename_all = "snake_case")]
+#[derive(Default)]
+pub enum ComixTypeFilter {
+	#[default]
+	Manga,
+	Manhwa,
+	Manhua,
+	Other,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum Tag {
+	Action,
+	Adult,
+	Adventure,
+	BoysLove,
+	Comedy,
+	Crime,
+	Drama,
+	Ecchi,
+	Fantasy,
+	GirlsLove,
+	Hentai,
+	Historical,
+	Horror,
+	Isekai,
+	MagicalGirls,
+	Mature,
+	Mecha,
+	Medical,
+	Mystery,
+	Philosophical,
+	Psychological,
+	Romance,
+	SciFi,
+	SliceOfLife,
+	Smut,
+	Sports,
+	Superhero,
+	Thriller,
+	Tragedy,
+	Wuxia,
+	Aliens,
+	Animals,
+	Cooking,
+	CrossDressing,
+	Delinquents,
+	Demons,
+	Genderswap,
+	Ghosts,
+	Gyaru,
+	Harem,
+	Incest,
+	Loli,
+	Mafia,
+	Magic,
+	MartialArts,
+	Military,
+	MonsterGirls,
+	Monsters,
+	Music,
+	Ninja,
+	OfficeWorkers,
+	Police,
+	PostApocalyptic,
+	Reincarnation,
+	ReverseHarem,
+	Samurai,
+	SchoolLife,
+	Shota,
+	Supernatural,
+	Survival,
+	TimeTravel,
+	TraditionalGames,
+	Vampires,
+	VideoGames,
+	Villainess,
+	VirtualReality,
+	Zombies,
+}
+
+impl fmt::Display for Tag {
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		f.write_str(self.name())
+	}
+}
+
+impl Tag {
+	/// The numeric term_id used by the API.
+	pub const fn id(self) -> u32 {
+		match self {
+			Tag::Action => 6,
+			Tag::Adult => 87264,
+			Tag::Adventure => 7,
+			Tag::BoysLove => 8,
+			Tag::Comedy => 9,
+			Tag::Crime => 10,
+			Tag::Drama => 11,
+			Tag::Ecchi => 87265,
+			Tag::Fantasy => 12,
+			Tag::GirlsLove => 13,
+			Tag::Hentai => 87266,
+			Tag::Historical => 14,
+			Tag::Horror => 15,
+			Tag::Isekai => 16,
+			Tag::MagicalGirls => 17,
+			Tag::Mature => 87267,
+			Tag::Mecha => 18,
+			Tag::Medical => 19,
+			Tag::Mystery => 20,
+			Tag::Philosophical => 21,
+			Tag::Psychological => 22,
+			Tag::Romance => 23,
+			Tag::SciFi => 24,
+			Tag::SliceOfLife => 25,
+			Tag::Smut => 87268,
+			Tag::Sports => 26,
+			Tag::Superhero => 27,
+			Tag::Thriller => 28,
+			Tag::Tragedy => 29,
+			Tag::Wuxia => 30,
+			Tag::Aliens => 31,
+			Tag::Animals => 32,
+			Tag::Cooking => 33,
+			Tag::CrossDressing => 34,
+			Tag::Delinquents => 35,
+			Tag::Demons => 36,
+			Tag::Genderswap => 37,
+			Tag::Ghosts => 38,
+			Tag::Gyaru => 39,
+			Tag::Harem => 40,
+			Tag::Incest => 41,
+			Tag::Loli => 42,
+			Tag::Mafia => 43,
+			Tag::Magic => 44,
+			Tag::MartialArts => 45,
+			Tag::Military => 46,
+			Tag::MonsterGirls => 47,
+			Tag::Monsters => 48,
+			Tag::Music => 49,
+			Tag::Ninja => 50,
+			Tag::OfficeWorkers => 51,
+			Tag::Police => 52,
+			Tag::PostApocalyptic => 53,
+			Tag::Reincarnation => 54,
+			Tag::ReverseHarem => 55,
+			Tag::Samurai => 56,
+			Tag::SchoolLife => 57,
+			Tag::Shota => 58,
+			Tag::Supernatural => 59,
+			Tag::Survival => 60,
+			Tag::TimeTravel => 61,
+			Tag::TraditionalGames => 62,
+			Tag::Vampires => 63,
+			Tag::VideoGames => 64,
+			Tag::Villainess => 65,
+			Tag::VirtualReality => 66,
+			Tag::Zombies => 67,
+		}
+	}
+
+	pub const fn from_id(id: i64) -> Option<Self> {
+		match id {
+			6 => Some(Tag::Action),
+			87264 => Some(Tag::Adult),
+			7 => Some(Tag::Adventure),
+			8 => Some(Tag::BoysLove),
+			9 => Some(Tag::Comedy),
+			10 => Some(Tag::Crime),
+			11 => Some(Tag::Drama),
+			87265 => Some(Tag::Ecchi),
+			12 => Some(Tag::Fantasy),
+			13 => Some(Tag::GirlsLove),
+			87266 => Some(Tag::Hentai),
+			14 => Some(Tag::Historical),
+			15 => Some(Tag::Horror),
+			16 => Some(Tag::Isekai),
+			17 => Some(Tag::MagicalGirls),
+			87267 => Some(Tag::Mature),
+			18 => Some(Tag::Mecha),
+			19 => Some(Tag::Medical),
+			20 => Some(Tag::Mystery),
+			21 => Some(Tag::Philosophical),
+			22 => Some(Tag::Psychological),
+			23 => Some(Tag::Romance),
+			24 => Some(Tag::SciFi),
+			25 => Some(Tag::SliceOfLife),
+			87268 => Some(Tag::Smut),
+			26 => Some(Tag::Sports),
+			27 => Some(Tag::Superhero),
+			28 => Some(Tag::Thriller),
+			29 => Some(Tag::Tragedy),
+			30 => Some(Tag::Wuxia),
+			31 => Some(Tag::Aliens),
+			32 => Some(Tag::Animals),
+			33 => Some(Tag::Cooking),
+			34 => Some(Tag::CrossDressing),
+			35 => Some(Tag::Delinquents),
+			36 => Some(Tag::Demons),
+			37 => Some(Tag::Genderswap),
+			38 => Some(Tag::Ghosts),
+			39 => Some(Tag::Gyaru),
+			40 => Some(Tag::Harem),
+			41 => Some(Tag::Incest),
+			42 => Some(Tag::Loli),
+			43 => Some(Tag::Mafia),
+			44 => Some(Tag::Magic),
+			45 => Some(Tag::MartialArts),
+			46 => Some(Tag::Military),
+			47 => Some(Tag::MonsterGirls),
+			48 => Some(Tag::Monsters),
+			49 => Some(Tag::Music),
+			50 => Some(Tag::Ninja),
+			51 => Some(Tag::OfficeWorkers),
+			52 => Some(Tag::Police),
+			53 => Some(Tag::PostApocalyptic),
+			54 => Some(Tag::Reincarnation),
+			55 => Some(Tag::ReverseHarem),
+			56 => Some(Tag::Samurai),
+			57 => Some(Tag::SchoolLife),
+			58 => Some(Tag::Shota),
+			59 => Some(Tag::Supernatural),
+			60 => Some(Tag::Survival),
+			61 => Some(Tag::TimeTravel),
+			62 => Some(Tag::TraditionalGames),
+			63 => Some(Tag::Vampires),
+			64 => Some(Tag::VideoGames),
+			65 => Some(Tag::Villainess),
+			66 => Some(Tag::VirtualReality),
+			67 => Some(Tag::Zombies),
+			_ => None,
+		}
+	}
+
+	pub const fn name(self) -> &'static str {
+		match self {
+			Tag::Action => "Action",
+			Tag::Adult => "Adult",
+			Tag::Adventure => "Adventure",
+			Tag::BoysLove => "Boys Love",
+			Tag::Comedy => "Comedy",
+			Tag::Crime => "Crime",
+			Tag::Drama => "Drama",
+			Tag::Ecchi => "Ecchi",
+			Tag::Fantasy => "Fantasy",
+			Tag::GirlsLove => "Girls Love",
+			Tag::Hentai => "Hentai",
+			Tag::Historical => "Historical",
+			Tag::Horror => "Horror",
+			Tag::Isekai => "Isekai",
+			Tag::MagicalGirls => "Magical Girls",
+			Tag::Mature => "Mature",
+			Tag::Mecha => "Mecha",
+			Tag::Medical => "Medical",
+			Tag::Mystery => "Mystery",
+			Tag::Philosophical => "Philosophical",
+			Tag::Psychological => "Psychological",
+			Tag::Romance => "Romance",
+			Tag::SciFi => "Sci-Fi",
+			Tag::SliceOfLife => "Slice of Life",
+			Tag::Smut => "Smut",
+			Tag::Sports => "Sports",
+			Tag::Superhero => "Superhero",
+			Tag::Thriller => "Thriller",
+			Tag::Tragedy => "Tragedy",
+			Tag::Wuxia => "Wuxia",
+			Tag::Aliens => "Aliens",
+			Tag::Animals => "Animals",
+			Tag::Cooking => "Cooking",
+			Tag::CrossDressing => "Cross Dressing",
+			Tag::Delinquents => "Delinquents",
+			Tag::Demons => "Demons",
+			Tag::Genderswap => "Genderswap",
+			Tag::Ghosts => "Ghosts",
+			Tag::Gyaru => "Gyaru",
+			Tag::Harem => "Harem",
+			Tag::Incest => "Incest",
+			Tag::Loli => "Loli",
+			Tag::Mafia => "Mafia",
+			Tag::Magic => "Magic",
+			Tag::MartialArts => "Martial Arts",
+			Tag::Military => "Military",
+			Tag::MonsterGirls => "Monster Girls",
+			Tag::Monsters => "Monsters",
+			Tag::Music => "Music",
+			Tag::Ninja => "Ninja",
+			Tag::OfficeWorkers => "Office Workers",
+			Tag::Police => "Police",
+			Tag::PostApocalyptic => "Post-Apocalyptic",
+			Tag::Reincarnation => "Reincarnation",
+			Tag::ReverseHarem => "Reverse Harem",
+			Tag::Samurai => "Samurai",
+			Tag::SchoolLife => "School Life",
+			Tag::Shota => "Shota",
+			Tag::Supernatural => "Supernatural",
+			Tag::Survival => "Survival",
+			Tag::TimeTravel => "Time Travel",
+			Tag::TraditionalGames => "Traditional Games",
+			Tag::Vampires => "Vampires",
+			Tag::VideoGames => "Video Games",
+			Tag::Villainess => "Villainess",
+			Tag::VirtualReality => "Virtual Reality",
+			Tag::Zombies => "Zombies",
+		}
+	}
+}
+
+impl<'de> Deserialize<'de> for Tag {
+	fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+	where
+		D: Deserializer<'de>,
+	{
+		struct TagVisitor;
+
+		impl<'de> Visitor<'de> for TagVisitor {
+			type Value = Tag;
+
+			fn expecting(&self, f: &mut fmt::Formatter) -> fmt::Result {
+				f.write_str("a numeric tag id (i64)")
+			}
+
+			fn visit_i64<E>(self, v: i64) -> Result<Tag, E>
+			where
+				E: de::Error,
+			{
+				Tag::from_id(v).ok_or_else(|| E::custom(format!("unknown tag id: {v}")))
+			}
+
+			fn visit_u64<E>(self, v: u64) -> Result<Tag, E>
+			where
+				E: de::Error,
+			{
+				let v = i64::try_from(v).map_err(|_| E::custom("tag id too large"))?;
+				self.visit_i64(v)
+			}
+		}
+
+		deserializer.deserialize_i64(TagVisitor)
+	}
 }
