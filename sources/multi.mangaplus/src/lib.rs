81421085a58165d77a881e73cfc3d3ea11d5721d
Kaic Bastidas <kaicbastidas@gmail.com>
2026-02-15 23:09:15 +0000

    feat(mangaplus): add auth params conditionally

--- sources/multi.mangaplus/src/lib.rs
+++ sources/multi.mangaplus/src/lib.rs
@@ -31,13 +31,17 @@ fn get_api_url() -> String {
 }
 
 fn build_auth_params() -> String {
-	format!(
-		"&os={}&os_ver={}&app_ver={}&secret={}",
-		settings::get_os(),
-		settings::get_os_ver(),
-		settings::get_app_ver(),
-		settings::get_secret()
-	)
+    if settings::get_mobile() {
+        format!(
+            "&os={}&os_ver={}&app_ver={}&secret={}",
+            settings::get_os(),
+            settings::get_os_ver(),
+            settings::get_app_ver(),
+            settings::get_secret()
+        )
+    } else {
+        String::new()
+    }
 }
 
 #[derive(Default)]
